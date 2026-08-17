fa99e76db819c244b31fda4307914b427c56814e
BLSNCD <2857585364@qq.com>
2026-08-17 09:38:02 +0800

    docs(examples): add OSPTEK SPDX header to example entry main.c

--- versions/OED154-12864W002-C24/examples/esp32s3-1.54-oled-128x64-spi-ssd1309-bringup/main/main.c
+++ versions/OED154-12864W002-C24/examples/esp32s3-1.54-oled-128x64-spi-ssd1309-bringup/main/main.c
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2026 OSPTEK
+ * SPDX-License-Identifier: CC-BY-4.0
+ *
+ * https://github.com/osptek
+ */
+
 #include "esp_err.h"
 #include "ssd1309.h"
 #include "ssd1309_anim.h"
